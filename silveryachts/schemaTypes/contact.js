edaa33e65be50e90fe6401ed5a75c2626ce6bcc7
udo popp <udopopp@MacBook-Pro-M3X-2.local>
2026-07-10 12:32:58 +0200

    feat: add contact page schema and astro route

---
+++ silveryachts/schemaTypes/contact.js
@@ -0,0 +1,27 @@
+export default {
+  name: 'contact',
+  title: 'Contact Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Page Title',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      title: 'Email Address',
+      type: 'string',
+    },
+    {
+      name: 'phone',
+      title: 'Phone Number',
+      type: 'string',
+    },
+    {
+      name: 'contactPerson',
+      title: 'Contact Person / Department',
+      type: 'text',
+    }
+  ]
+}
--- silveryachts/schemaTypes/index.js
+++ silveryachts/schemaTypes/index.js
@@ -1,3 +1,4 @@
 import yacht from './yacht'
+import contact from './contact' // 1. Hier importieren
 
-export const schemaTypes = [yacht]
+export const schemaTypes = [yacht, contact] // 2. Hier zum Array hinzufügen
